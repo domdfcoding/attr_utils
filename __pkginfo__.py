1fed4b71bd1e04eb16e890d439b153b1e97cd74e
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-08-14 22:54:11 +0100

    Bump version: 0.0.0 → 0.0.1

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -39,7 +39,7 @@ __copyright__ = """
 2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
 """
 
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 modname = "attr_utils"
 pypi_name = "attr_utils"
 import_name = "attr_utils"
