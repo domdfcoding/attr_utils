66b32954030458d691bfdc47bb90bb5872cc59bc
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-10-16 14:58:20 +0100

    Updated config files.

--- doc-source/conf.py
+++ doc-source/conf.py
@@ -10,15 +10,12 @@ import sys
 sys.path.append(os.path.abspath('.'))
 sys.path.append(os.path.abspath('..'))
 
+# this package
 from __pkginfo__ import __version__
-
-# User-configurable lines
-# End of user-configurable lines
 
 github_username = "domdfcoding"
 github_repository = "attr_utils"
 github_url = f"https://github.com/{github_username}/{github_repository}"
-
 
 rst_prolog = f""".. |pkgname| replace:: attr_utils
 .. |pkgname2| replace:: ``attr_utils``
@@ -34,24 +31,24 @@ language = 'en'
 package_root = "attr_utils"
 
 extensions = [
-	'sphinx_toolbox',
-	'sphinx_toolbox.more_autodoc',
-	'sphinx_toolbox.more_autosummary',
-	'sphinx_toolbox.tweaks.param_dash',
-	'sphinx.ext.intersphinx',
-	'sphinx.ext.mathjax',
-	'sphinxcontrib.httpdomain',
-	'sphinxcontrib.extras_require',
-	'sphinx.ext.todo',
-	'sphinxemoji.sphinxemoji',
-	'notfound.extension',
-	'sphinx_copybutton',
-	'sphinxcontrib.default_values',
-	'sphinxcontrib.toctree_plus',
-	'seed_intersphinx_mapping',
-	'attr_utils.annotations',
-	'attr_utils.autoattrs',
-	]
+		'sphinx_toolbox',
+		'sphinx_toolbox.more_autodoc',
+		'sphinx_toolbox.more_autosummary',
+		'sphinx_toolbox.tweaks.param_dash',
+		'sphinx.ext.intersphinx',
+		'sphinx.ext.mathjax',
+		'sphinxcontrib.httpdomain',
+		'sphinxcontrib.extras_require',
+		'sphinx.ext.todo',
+		'sphinxemoji.sphinxemoji',
+		'notfound.extension',
+		'sphinx_copybutton',
+		'sphinxcontrib.default_values',
+		'sphinxcontrib.toctree_plus',
+		'seed_intersphinx_mapping',
+		'attr_utils.annotations',
+		'attr_utils.autoattrs',
+		]
 
 sphinxemoji_style = 'twemoji'
 todo_include_todos = bool(os.environ.get("SHOW_TODOS", 0))
@@ -69,9 +66,6 @@ pygments_style = 'default'
 intersphinx_mapping = {
 		'python': ('https://docs.python.org/3/', None),
 		'sphinx': ('https://www.sphinx-doc.org/en/stable/', None),
-		'rtd': ('https://docs.readthedocs.io/en/latest/', None),
-		"h5py": ('https://docs.h5py.org/en/latest/', None),
-		"sarge": ('https://sarge.readthedocs.io/en/latest/', None),
 		}
 
 html_theme = 'domdf_sphinx_theme'
@@ -112,33 +106,36 @@ toctree_plus_types = {
 		}
 
 add_module_names = False
+hide_none_rtype = True
+all_typevars = True
+overloads_location = "bottom"
 
 
+autodoc_exclude_members = [   # Exclude "standard" methods.
+		"__dict__",
+		"__class__",
+		"__dir__",
+		"__weakref__",
+		"__module__",
+		"__annotations__",
+		"__orig_bases__",
+		"__parameters__",
+		"__subclasshook__",
+		"__init_subclass__",
+		"__attrs_attrs__",
+		"__init__",
+		"__new__",
+		"__getnewargs__",
+		"__abstractmethods__",
+		"__hash__",
+		]
 autodoc_default_options = {
 		'members': None,  # Include all members (methods).
 		'special-members': None,
 		"autosummary": None,
 		"show-inheritance": None,
-		'exclude-members': ','.join([   # Exclude "standard" methods.
-				"__dict__",
-				"__class__",
-				"__dir__",
-				"__weakref__",
-				"__module__",
-				"__annotations__",
-				"__orig_bases__",
-				"__parameters__",
-				"__subclasshook__",
-				"__init_subclass__",
-				"__attrs_attrs__",
-				"__init__",
-				"__new__",
-				"__getnewargs__",
-				"__abstractmethods__",
-				"__hash__",
-				]),
+		'exclude-members': ','.join(autodoc_exclude_members),
 		}
-
 
 set_type_checking_flag = True
 intersphinx_mapping['sphinx_toolbox'] = ('https://sphinx-toolbox.readthedocs.io/en/latest/', None)
