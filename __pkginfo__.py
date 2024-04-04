7ae7929560d743659e9fda664d8dccae354ba9aa
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2024-04-04 10:25:38 +0100

    Test on Sphinx 6.x

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -4,6 +4,6 @@ __all__ = ["extras_require"]
 
 extras_require = {
 		"pprint": ["prettyprinter==0.18.0"],
-		"sphinx": ["sphinx<6,>=3.2.0", "sphinx-toolbox>=2.16.0"],
-		"all": ["prettyprinter==0.18.0", "sphinx<6,>=3.2.0", "sphinx-toolbox>=2.16.0"]
+		"sphinx": ["sphinx<7,>=3.2.0", "sphinx-toolbox>=2.16.0"],
+		"all": ["prettyprinter==0.18.0", "sphinx<7,>=3.2.0", "sphinx-toolbox>=2.16.0"]
 		}
